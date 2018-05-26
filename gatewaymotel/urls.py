5425c3d5ab67cee0ee687543cc8d550b5595f8f0
LeetDit <heesoo@ualberta.ca>
2018-05-25 21:29:49 -0600

    final

--- gatewaymotel/urls.py
+++ gatewaymotel/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views #.은 현재 폴더(elections)를 의미합니다.
 
+app_name = 'gatewaymotel'
+
 urlpatterns = [
-    url(r'^$', views.index), #위의 urls.py와는 달리 include가 없습니다.
-]
+    url(r'^$', views.index, name = 'home'), #위의 urls.py와는 달리 include가 없습니다.
+    url(r'^index1/$', views.about, name = 'about'),
+    url(r'^index2/$', views.attractions, name = 'attract'),
+    url(r'^index3/$', views.room, name = 'room'),
+    url(r'^index4/$', views.contact, name = 'contact'),
+    ]
--- gatewaymotel/views.py
+++ gatewaymotel/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'gatewaymotel/index.html')
+
+def about(request):
+    return render(request, 'gatewaymotel/index1.html')
+
+def attractions(request):
+    return render(request, 'gatewaymotel/index2.html')
+
+def room(request):
+    return render(request, 'gatewaymotel/index3.html')
+
+def contact(request):
+    return render(request, 'gatewaymotel/index4.html')
